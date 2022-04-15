65109252f46c46d6e6c3abdb8202f4c362026be3
vivian <vivian.v18723@gmail.com>
2022-04-15 23:10:28 +0100

    add starter code for js functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,34 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("hello world");
+document.addEventListener("DOMContentLoaded", function () {
+  // changes images
+  const changeImage = () => {
+    // left arrow
+    const leftArrow = document.getElementById("left");
+
+    // clicking left arrow
+    leftArrow.addEventListener("click", function () {
+
+      // image src
+      const srcImg = document.getElementById("image").src;
+
+      // arr of images
+      const allImagesSrc = [
+        "images/pool.avif",
+        "images/palms.avif",
+        "images/umbrellas.avif",
+      ];
+
+      console.log("srcImg: ", srcImg);
+      console.log("allImagesSrc[0]: ", allImagesSrc[0]);
+      console.log("allImagesSrc[1]: ", allImagesSrc[1]);
+      console.log("allImagesSrc[2]: ", allImagesSrc[2]);
+
+      // loop through img arr
+      for (let i = 0; i < allImagesSrc.length; i++) {
+        if(srcImg === allImagesSrc[i]) {
+          console.log("SAME!!!");
+        }
+      }
+    });
+  };
+  changeImage();
 });
