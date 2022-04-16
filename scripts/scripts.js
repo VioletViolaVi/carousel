5f7b7076a5243bd3855b6843d63246e84b247160
vivian <vivian.v18723@gmail.com>
2022-04-16 01:05:08 +0100

    refactor code for left arrow & write to do list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const changeImage = () => {
-    const allImagesArr = [
-      "images/pool.avif", // 0
-      "images/palms.avif", // 1
-      "images/umbrellas.avif", // 2
-    ];
-
+  const changeLeftImage = () => {
+    // get left arrow id
     const leftArrow = document.getElementById("left");
 
+    // click function for left arrow
     leftArrow.addEventListener("click", function () {
-      // document.getElementById("imageId").src = allImagesArr[0];
+      // images in array
+      const allImagesArr = [
+        "http://127.0.0.1:5555/images/pool.avif", // index 0
+        "http://127.0.0.1:5555/images/umbrellas.avif", // index 1
+        "http://127.0.0.1:5555/images/palms.avif", // index 2
+      ];
 
-      if (document.getElementById("imageId").src === "http://127.0.0.1:5555/images/pool.avif") {
+      // toggles among images
+      if (document.getElementById("imageId").src === allImagesArr[0]) {
         document.getElementById("imageId").src = allImagesArr[2];
-      } else if (
-        document.getElementById("imageId").src === "http://127.0.0.1:5555/images/umbrellas.avif"
-      ) {
+      } else if (document.getElementById("imageId").src === allImagesArr[2]) {
         document.getElementById("imageId").src = allImagesArr[1];
-      } else if (
-        document.getElementById("imageId").src === "http://127.0.0.1:5555/images/palms.avif"
-      ) {
+      } else if (document.getElementById("imageId").src === allImagesArr[1]) {
         document.getElementById("imageId").src = allImagesArr[0];
-      } else{
-        console.log("ERROR!!!!");
       }
-
-      
-      console.log(document.getElementById("imageId").src);
     });
   };
-  changeImage();
+  changeLeftImage();
 });
 
 /* 
-  - change index (allImagesArr[0]) using this order:  [1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2 ]
+  TO DO LIST:
+  - toggle index (allImagesArr[0]) using this order:  [1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2 ]
   - from zero (LEFT arrow):
     - add 2
     - subtract 1
@@ -44,4 +38,12 @@ document.addEventListener("DOMContentLoaded", function () {
     - add 1
     - subtract 2
     - and so on....
+  - toggle alt:
+    - alt="Hotel front with palm trees and a pool." (palms)
+    - alt="Hotel in background with umbrellas and decks in front." (umbrella)
+  - toggle text:
+    - <figcaption>Peaceful green spaces</figcaption> (palms)
+    - <figcaption>Relax under the shade</figcaption> (umbrella)
+  - repeat function for right arrow
+  - make responsive
 */
