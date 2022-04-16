927b297add03690685690bf56328bdee052eb8e1
vivian <vivian.v18723@gmail.com>
2022-04-16 17:20:20 +0100

    place image data in own js file

---
+++ styles/imgData.js
@@ -0,0 +1,23 @@
+//   captions & links for images
+const allImageInfo = [
+  {
+    id: 0,
+    imgLink: "http://127.0.0.1:5555/images/pool.avif",
+    caption: "Chill by the pool with orange cushions",
+    altText: "Hotel with pool & orange cushions.",
+  },
+
+  {
+    id: 1,
+    imgLink: "http://127.0.0.1:5555/images/umbrellas.avif",
+    caption: "Home away from home",
+    altText: "Hotel with clear skies",
+  },
+
+  {
+    id: 2,
+    imgLink: "http://127.0.0.1:5555/images/palms.avif",
+    caption: "Tropical paradise with cloudy skies",
+    altText: "Hotel tropical paradise with cloudy skies",
+  },
+];
